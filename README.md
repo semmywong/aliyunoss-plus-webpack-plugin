a676fb4e8141244d32289d58244284ec879ad8c9
黄世民 <948959@nd.com>
2020-09-26 21:14:26 +0800

    [Feature]添加deletePrefix

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-##aliyunoss-webpack-plugin
+##aliyunoss-plus-webpack-plugin
 webpack的插件，用于自动上传静态资源到阿里的oss上，以便作为静态资源使用，当然你也可以用于自动存储大文件。  
 
 Installation
 ------------
 Install the plugin with npm:
-```shell
-$ npm install aliyunoss-webpack-plugin --save-dev
+
+``` shell
+$ npm install aliyunoss-plus-webpack-plugin --save-dev
 ```
 
 Basic Usage
@@ -14,14 +15,17 @@ Basic Usage
 add the plugin to your webpack config as follows:
 
 ```javascript
-var AliyunossWebpackPlugin = require('aliyunoss-webpack-plugin')
+var AliyunossWebpackPlugin = require('aliyunoss-plus-webpack-plugin')
 var webpackConfig = {
-  entry: 'index.js',
+  entry: 'index.js', 
   output: {
+
     path: 'dist',
     filename: 'index_bundle.js'
-  },
+
+  }, 
   plugins: [new AliyunossWebpackPlugin({
+
     buildPath:'your build path',
     region: 'your region',
     accessKeyId: 'your key',
@@ -32,29 +36,35 @@ var webpackConfig = {
         Expires: 6000
       }
     }
+
   })]
 }
 ```   
 
-don't depend webpack,just as follows:
+don't depend webpack, just as follows:
 ```javascript
-var AliyunossWebpackPlugin = require('aliyunoss-webpack-plugin')
+var AliyunossWebpackPlugin = require('aliyunoss-plus-webpack-plugin')
 var oss = new AliyunossWebpackPlugin({
-  buildPath:'your build path',
-  region: 'your region',
-  accessKeyId: 'your key',
-  accessKeySecret: 'your secret',
-  bucket: 'your bucket',
-  deleteAll: true,
+  buildPath:'your build path', 
+  region: 'your region', 
+  accessKeyId: 'your key', 
+  accessKeySecret: 'your secret', 
+  bucket: 'your bucket', 
+  deletePrefix: 'prefix/', 
+  deleteAll: true, 
   generateObjectPath: function(filename) {
+
     return filename
-  },
+
+  }, 
   getObjectHeaders: function(filename) {
+
     return {
       Expires: 6000
     }
+
   }
-});
+}); 
 oss.apply()
 ```     
 
@@ -62,12 +72,13 @@ Configuration
 -------------
 The plugin allowed values are as follows:
 
-- `buildPath`: 需要上传的文件路径,支持整个文件夹的遍历。支持node-glob风格路径，具体可以参见[node-glob的文档](https://github.com/isaacs/node-glob)。
-- `region`: oss的区域，如:oss-cn-shanghai。
-- `accessKeyId`: 阿里云的权限访问的key。
-- `accessKeySecret`: 阿里云的权限访问的secret。
-- `bucket`: 阿里云OSS上的命名空间。
-- `deleteAll`: 先删除oss上的代码之后再上传，默认为false
-- `generateObjectPath`: 函数（可选），函数参数为上传的文件名，必须返回一个字符串作为文件名，默认为文件原名。通过该函数可以让使用者自定义上传的文件名或者修改oss的路径，比如加一个前缀等
-- `getObjectHeaders`: 函数（可选），函数参数为上传的文件名，返回设置的headers对象，默认为空，具体可以参见[ali-oss的文档](http://doc.oss.aliyuncs.com/#_Toc336676772)
-- `internal`: 使用内网还是外网（可选），默认外网
+* `buildPath` : 需要上传的文件路径, 支持整个文件夹的遍历。支持node-glob风格路径，具体可以参见[node-glob的文档](https://github.com/isaacs/node-glob)。
+* `region` : oss的区域，如:oss-cn-shanghai。
+* `accessKeyId` : 阿里云的权限访问的key。
+* `accessKeySecret` : 阿里云的权限访问的secret。
+* `bucket` : 阿里云OSS上的命名空间。
+* `deletePrefix` : 要删除的前缀，与deleteAll配合使用，默认是''
+* `deleteAll` : 先删除oss上的代码之后再上传，默认为false
+* `generateObjectPath` : 函数（可选），函数参数为上传的文件名，必须返回一个字符串作为文件名，默认为文件原名。通过该函数可以让使用者自定义上传的文件名或者修改oss的路径，比如加一个前缀等
+* `getObjectHeaders` : 函数（可选），函数参数为上传的文件名，返回设置的headers对象，默认为空，具体可以参见[ali-oss的文档](http://doc.oss.aliyuncs.com/#_Toc336676772)
+* `internal` : 使用内网还是外网（可选），默认外网
--- index.js
+++ index.js
@@ -4,97 +4,137 @@ var fs = require('fs');
 var oss = require('ali-oss');
 var co = require('co');
 var colors = require('colors');
-var fileUtils = require('./utils/file')
-var _ = require('./utils/object')
-
+var fileUtils = require('./utils/file');
+var _ = require('./utils/object');
 
 function AliyunossWebpackPlugin(options) {
-	if (!options || !options.buildPath || !options.region || !options.accessKeyId || !options.accessKeySecret || !options.bucket) {
-		console.info('Some parameters of the problem，please set as the follow:')
-		console.info(" new".red + " AliyunossWebpackPlugin({");
-		console.info("   buildPath:'your path',".yellow);
-		console.info("   region: 'your region',".yellow);
-		console.info("   accessKeyId: 'your accessKeyId',".yellow);
-		console.info("   accessKeySecret: 'your accessKeySecret',".yellow);
-		console.info("   bucket: 'your bucket'".yellow);
-		console.info(" })");
-		throw new Error('Some parameters of the problem')
-	}
-	this.fileArray = [];
-	this.options = _.extend({
-
-	}, options);
+    if (
+        !options ||
+        !options.buildPath ||
+        !options.region ||
+        !options.accessKeyId ||
+        !options.accessKeySecret ||
+        !options.bucket
+    ) {
+        console.info(
+            'Some parameters of the problem，please set as the follow:'
+        );
+        console.info(' new'.red + ' AliyunossWebpackPlugin({');
+        console.info("   buildPath:'your path',".yellow);
+        console.info("   region: 'your region',".yellow);
+        console.info("   accessKeyId: 'your accessKeyId',".yellow);
+        console.info("   accessKeySecret: 'your accessKeySecret',".yellow);
+        console.info("   bucket: 'your bucket'".yellow);
+        console.info(' })');
+        throw new Error('Some parameters of the problem');
+    }
+    this.fileArray = [];
+    this.options = _.extend({}, options);
 }
 
-AliyunossWebpackPlugin.prototype.apply = function(compiler) {
-	var _this = this;
-	if (compiler) {
-		compiler.plugin("done", function(compilation) {
-			_this.oposs();
-		});
-	} else {
-		_this.oposs();
-	}
+AliyunossWebpackPlugin.prototype.apply = function (compiler) {
+    var _this = this;
+    if (compiler) {
+        compiler.plugin('done', function (compilation) {
+            _this.oposs();
+        });
+    } else {
+        _this.oposs();
+    }
 };
 
-AliyunossWebpackPlugin.prototype.oposs = function() {
-	var _this = this;
-	var deleteAll = _this.options.deleteAll || false;
-	var generateObjectPath = _this.options.generateObjectPath || function(fileName) {
-		return fileName;
-	}
-	var getObjectHeaders = _this.options.getObjectHeaders || function() {
-		return {};
-	}
+AliyunossWebpackPlugin.prototype.oposs = function () {
+    var _this = this;
+    var deleteAll = _this.options.deleteAll || false;
+    var deletePrefix = _this.options.deletePrefix || '';
+    var generateObjectPath =
+        _this.options.generateObjectPath ||
+        function (fileName) {
+            return fileName;
+        };
+    var getObjectHeaders =
+        _this.options.getObjectHeaders ||
+        function () {
+            return {};
+        };
 
-	co(function*() {
-		'use strict';
-		var store = oss({
-			region: _this.options.region,
-			accessKeyId: _this.options.accessKeyId,
-			accessKeySecret: _this.options.accessKeySecret,
-			bucket: _this.options.bucket,
-			internal: _this.options.internal ? true : false,
-		});
+    co(function* () {
+        'use strict';
+        var store = oss({
+            region: _this.options.region,
+            accessKeyId: _this.options.accessKeyId,
+            accessKeySecret: _this.options.accessKeySecret,
+            bucket: _this.options.bucket,
+            internal: _this.options.internal ? true : false,
+        });
 
-		//删除oss上代码
-		if (deleteAll) {
-			var fileList = yield store.list();
-			var files = [];
-			if (fileList.objects) {
-				fileList.objects.forEach(function(file) {
-					files.push(file.name);
-				})
-				var result = yield store.deleteMulti(files, {
-					quiet: true
-				});
-			}
-		}
+        //删除oss上代码
+        if (deleteAll) {
+            if (deletePrefix) {
+                var prefixList = yield store.list({
+                    prefix: deletePrefix,
+                });
+                prefixList.objects = prefixList.objects || [];
+                yield Promise.all(
+                    prefixList.objects.map((file) => {
+                        co(function* () {
+                            try {
+                                yield store.delete(file.name);
+                                console.log(
+                                    'delete '.red + file.name + ' success'.green
+                                );
+                            } catch (error) {
+                                console.log(
+                                    'delete '.red + file.name + ' fail'.red
+                                );
+                                error.failObjectName = name;
+                                return error;
+                            }
+                        });
+                    })
+                );
+            } else {
+                var fileList = yield store.list();
+                var files = [];
+                if (fileList.objects) {
+                    fileList.objects.forEach(function (file) {
+                        files.push(file.name);
+                    });
+                    var result = yield store.deleteMulti(files, {
+                        quiet: true,
+                    });
+                }
+            }
+        }
 
-		//上传oss的新代码
-		fileUtils.eachFileSync(_this.options.buildPath, function(filename, stats) {
-			_this.fileArray.push(filename);
-		});
+        //上传oss的新代码
+        fileUtils.eachFileSync(_this.options.buildPath, function (
+            filename,
+            stats
+        ) {
+            _this.fileArray.push(filename);
+        });
 
-		var j = 0;
-		for (var i = 0; i < _this.fileArray.length; i++) {
-			var file = _this.fileArray[i];
-			var fileName = file.split('/').pop();
-			var ossFileName = generateObjectPath(fileName, file);
-			if (ossFileName) {
-				yield store.put(ossFileName, file, {
-					timeout: _this.options.timeout,
-					headers: getObjectHeaders(fileName)
-				});
-				console.log(file + ' -- upload to ' + ossFileName + ' success'.green);
-			} else {
-				console.log('skipping file ' + file)
-			}
-		}
-	}).catch(function(err) {
-		console.info(err)
-	})
-}
-
+        var j = 0;
+        for (var i = 0; i < _this.fileArray.length; i++) {
+            var file = _this.fileArray[i];
+            var fileName = file.split('/').pop();
+            var ossFileName = generateObjectPath(fileName, file);
+            if (ossFileName) {
+                yield store.put(ossFileName, file, {
+                    timeout: _this.options.timeout,
+                    headers: getObjectHeaders(fileName),
+                });
+                console.log(
+                    file + ' -- upload to ' + ossFileName + ' success'.green
+                );
+            } else {
+                console.log('skipping file ' + file);
+            }
+        }
+    }).catch(function (err) {
+        console.info(err);
+    });
+};
 
 module.exports = AliyunossWebpackPlugin;
